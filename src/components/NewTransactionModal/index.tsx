d116c0980568b7d9686131bc346c6b2161902ee4
DevSP <contatodevsp@gmail.com>
2021-03-24 22:00:47 -0300

    Salvando dados do form na API

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import Modal from 'react-modal'
+
+import { api } from '../../services/api'
 
 import { Container, TransactionTypeContainer, RadioButton } from './styles'
 
@@ -15,7 +17,23 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const [title, setTitle] = useState('')
+    const [value, setValue] = useState(0)
     const [type, setType] = useState('deposit')
+    const [category, setCategory] = useState('')
+
+    function handleCreateNewTransaction(event: FormEvent) {
+        event.preventDefault()
+
+        const data = {
+            title,
+            value,
+            type,
+            category
+        }
+
+        api.post('/transactions', data)
+    }
 
     return (
         <Modal
@@ -28,12 +46,12 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 <img src={closeImg} alt="Fechar Modal" />
             </button>
 
-            <Container>
+            <Container onSubmit={handleCreateNewTransaction}>
                 <h2>Cadastrar transação</h2>
 
-                <input placeholder="Título" />
+                <input placeholder="Título" value={title} onChange={e => setTitle(e.target.value)} />
 
-                <input placeholder="Valor" type="number" />
+                <input placeholder="Valor" type="number" value={value} onChange={e => setValue(Number(e.target.value))} />
 
                 <TransactionTypeContainer>
                     <RadioButton type="button" onClick={() => setType('deposit')} isActive={type === 'deposit'} activeColor='green'>
@@ -47,7 +65,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                     </RadioButton>
                 </TransactionTypeContainer>
 
-                <input placeholder="Categoria" />
+                <input placeholder="Categoria" value={category} onChange={e => setCategory(e.target.value)} />
 
                 <button type="submit">Cadastrar</button>
             </Container>
